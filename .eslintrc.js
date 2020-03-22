c31ae325481ef98680bd39905579fe96cc8bb47a
Simon Sinclair <simon@simonsinclair.com>
2020-03-22 22:42:22 +0000

    Install eslint

---
+++ .eslintrc.js
@@ -0,0 +1,11 @@
+module.exports = {
+  extends: [
+    'airbnb-base',
+  ],
+  env: {
+    browser: true,
+    // "Enable all ECMAScript 6 features except for modules (this
+    // automatically sets the ecmaVersion parser option to 6)."
+    es6: true,
+  },
+};
